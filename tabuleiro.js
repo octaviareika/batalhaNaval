9637248a85d13544720a01f4cba9997201761da2
octaviareika <ceciliaperet81@gmail.com>
2024-04-07 21:04:12 -0300

    finalizando

--- tabuleiro.js
+++ tabuleiro.js
@@ -1,6 +1,8 @@
 // colocar as funções de tabuleiro aqui
 
 function exibeTab(tabuleiro, mascara){
+    console.log('-----------------------------------------'); // linha separadora
+
     for (let i = 0; i < mascara.length; i++) {
         console.log('| ' + mascara[i].join(' | ') + ' |'); // join = separa os elementos do array por um caracter
         console.log('-----------------------------------------'); // linha separadora
